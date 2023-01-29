3fad17a286f3a4584ac1566e9288189c89f1a5e0
lfurmanchuk <lesyazhuruk@gmail.com>
2023-01-30 00:36:38 +0200

    util

---
+++ src/components/BlogCard/BlogCard.jsx
@@ -0,0 +1,19 @@
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  CardPoster,
+  CardText,
+  CardTitle,
+  CardFooter,
+  Tag,
+  Avatar,
+  UserBox,
+  UserInfo,
+  UserName,
+  DateElem,
+} from './BlogCard.styled';
+
+export const BlogCard = () => {
+  return <div>BlogCard</div>;
+};
